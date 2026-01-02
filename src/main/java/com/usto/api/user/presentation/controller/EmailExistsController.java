9c9c8f3490614e22c90318e6d30f856681047e10
parkdohyun <badberg2002@gmail.com>
2026-01-03 03:09:36 +0900

    url 최대한 세분화

--- src/main/java/com/usto/api/user/presentation/controller/EmailExistsController.java
+++ src/main/java/com/usto/api/user/presentation/controller/EmailExistsController.java
@@ -3,24 +3,24 @@ package com.usto.api.user.presentation.controller;
 import com.usto.api.common.utils.ApiResponse;
 import com.usto.api.user.application.EmailExistsApplication;
 import com.usto.api.user.presentation.dto.response.EmailExistsResponseDto;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users/exists")
+
 public class EmailExistsController {
 
     private final EmailExistsApplication emailExistsApplication;
 
-    @GetMapping("/api/users/exists")
+    @GetMapping("/email")
     public ApiResponse<?> existsByEmail(
-            @RequestParam(value = "email", required = false) String email
+            @RequestParam(required = false)
+            String email
     ) {
         if (email == null || email.trim().isEmpty()) {
             return ApiResponse.fail("이메일을 써주세요");
