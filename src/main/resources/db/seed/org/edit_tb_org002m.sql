22ccbe6de610ace99166cdd365a3dd52d76ae0c5
yeyoungyun <dpdudyyy@gmail.com>
2026-01-08 05:45:17 +0900

    feat: 테스트용 운용부서 데이터 추가

--- src/main/resources/db/seed/org/edit_tb_org002m.sql
+++ src/main/resources/db/seed/org/edit_tb_org002m.sql
@@ -16,6 +16,19 @@ INSERT INTO TB_ORG002M (
 -- 단과대학 계열
 ('HANYANG_ERICA', 'COL_SW_RC',  '소프트웨어융합대학RC 행정팀', '단과대학',   'SYSTEM');
 
+-- 타 조직의 운용부서 (운용부서 api 테스트용으로 추가함)
+INSERT INTO TB_ORG002M (
+    ORG_CD,
+    DEPT_CD,
+    DEPT_NM,
+    DEPT_TY,
+    CRE_BY
+) VALUES
+('HANYANG_SEOUL', 'LIB_PLAN', '학술기획운영팀', '백남학술정보관', 'SYSTEM'),
+('HANYANG_SEOUL', 'LIB_RESEARCH', '연구정보팀', '백남학술정보관', 'SYSTEM'),
+('HANYANG_SEOUL', 'ADM_FAC',  '시설팀', '관리처', 'SYSTEM'),
+('HANYANG_SEOUL', 'ADM_ASSET',  '관재팀', '관리처', 'SYSTEM');
+
 -- 확인
 SELECT *
 FROM TB_ORG002M
