19f98a282601a251e84c3c7ac0b5bd1f3c23fac8
parkdohyun <badberg2002@gmail.com>
2026-01-08 19:34:26 +0900

    회원 정보 일치 여부 확인을 위한 Dto 개선

--- src/main/java/com/usto/api/user/presentation/dto/request/PasswordFindRequestDto.java
+++ src/main/java/com/usto/api/user/presentation/dto/request/PasswordFindRequestDto.java
@@ -1,5 +1,6 @@
 package com.usto.api.user.presentation.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -8,4 +9,7 @@ public class PasswordFindRequestDto {
 
     @NotBlank
     String usrId;
+
+    @Email
+    String email;
 }
--- src/main/java/com/usto/api/user/presentation/dto/request/UserIdFindRequestDto.java
+++ src/main/java/com/usto/api/user/presentation/dto/request/UserIdFindRequestDto.java
@@ -1,5 +1,6 @@
 package com.usto.api.user.presentation.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -8,4 +9,8 @@ public class UserIdFindRequestDto {
 
     @NotBlank
     String usrNm;
+
+    @Email
+    String email;
+
 }
