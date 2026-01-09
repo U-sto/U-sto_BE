82cf4e31b6eb0425242b5bb63762f576330de283
parkdohyun <badberg2002@gmail.com>
2026-01-10 01:07:36 +0900

    컨트롤러 구현

--- src/main/java/com/usto/api/user/presentation/controller/UserController.java
+++ src/main/java/com/usto/api/user/presentation/controller/UserController.java
@@ -2,15 +2,16 @@ package com.usto.api.user.presentation.controller;
 
 import com.usto.api.common.utils.ApiResponse;
 import com.usto.api.user.application.SignupApplication;
+import com.usto.api.user.application.UserUpdateApplication;
 import com.usto.api.user.presentation.dto.request.SignupRequestDto;
+import com.usto.api.user.presentation.dto.request.UserUpdateRequestDto;
+import com.usto.api.user.presentation.dto.response.UserUpdateResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "user-controller", description = "회원 정보 관련 API")
 @Slf4j
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final SignupApplication signupApplication;
+    private final UserUpdateApplication userUpdateApplication;
 
     @PostMapping("/api/users")
     @Operation(summary = "회원 가입")
@@ -48,5 +50,17 @@ public class UserController {
         session.removeAttribute("signup.preauth.expiresAt");
 
         return ApiResponse.ok("회원가입 완료");
+    }
+
+    @PutMapping("/{userId}")
+    @Operation(summary = "회원 수정")
+    public ApiResponse<UserUpdateResponseDto> updateUser(
+            @PathVariable String userId,
+            @RequestBody UserUpdateRequestDto request
+    ) {
+        return ApiResponse.ok(
+                "회원정보가 정상적으로 수정되었습니다.",
+                userUpdateApplication.update(userId, request)
+        );
     }
 }
