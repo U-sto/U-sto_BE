11336832c8eb381581ca466da1ef12679cfa7d6d
parkdohyun <badberg2002@gmail.com>
2026-01-10 03:30:35 +0900

    세션 로직을 컨트롤러에서 구현, 어플리케이션은 단순 조회만

--- src/main/java/com/usto/api/user/presentation/controller/LogController.java
+++ src/main/java/com/usto/api/user/presentation/controller/LogController.java
@@ -7,12 +7,25 @@ import com.usto.api.user.presentation.dto.request.LoginRequestDto;
 import com.usto.api.user.presentation.dto.response.LoginResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "log-controller", description = "로그인/아웃 관련 API")
 @RestController
@@ -20,23 +33,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class LogController {
 
+    private final AuthenticationManager authenticationManager; //세션 토큰 관리자님
     private final LoginApplication loginApplication;
+
+    private final SecurityContextRepository securityContextRepository =
+            new HttpSessionSecurityContextRepository();
 
     @PostMapping("/login")
     @Operation(summary = "로그인")
     public ApiResponse<?> login(
-            @Valid @RequestBody LoginRequestDto request
+            @Valid @RequestBody LoginRequestDto request,
+            HttpServletRequest httpRequest,
+            HttpServletResponse httpResponse
     ) {
-        LoginUser user = loginApplication.login(
-                request.getUsrId(),
-                request.getPwd()
+
+        // 비밀번호 검증 + 상태체크
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsrId(), request.getPwd())
+        );
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+
+        // 세션에 저장
+        securityContextRepository.saveContext(context, httpRequest, httpResponse);
+
+        LoginUser loginUser = loginApplication.login(
+                request.getUsrId()
         );
 
         return ApiResponse.ok(
                 "로그인 성공",
                 new LoginResponseDto(
-                        user.getUsrId(),
-                        user.getUsrNm()
+                        loginUser.getUsrId(),
+                        loginUser.getUsrNm()
                 )
         );
     }
