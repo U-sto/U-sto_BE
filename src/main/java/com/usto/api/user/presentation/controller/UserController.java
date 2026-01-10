0aed8fa27646240396487a2fc71bce89bb6a0d72
parkdohyun <badberg2002@gmail.com>
2026-01-10 18:52:28 +0900

    updateProfile 추가 및 최적화

--- src/main/java/com/usto/api/user/presentation/controller/UserController.java
+++ src/main/java/com/usto/api/user/presentation/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.usto.api.user.presentation.controller;
 
+import com.usto.api.common.exception.BusinessException;
 import com.usto.api.common.utils.ApiResponse;
 import com.usto.api.user.application.SignupApplication;
 import com.usto.api.user.application.UserUpdateApplication;
+import com.usto.api.user.domain.UserPrincipal;
+import com.usto.api.user.domain.model.LoginUser;
 import com.usto.api.user.presentation.dto.request.SignupRequestDto;
 import com.usto.api.user.presentation.dto.request.UserUpdateRequestDto;
+import com.usto.api.user.presentation.dto.response.LoginResponseDto;
 import com.usto.api.user.presentation.dto.response.UserUpdateResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "user-controller", description = "회원 정보 관련 API")
@@ -22,7 +28,7 @@ public class UserController {
     private final SignupApplication signupApplication;
     private final UserUpdateApplication userUpdateApplication;
 
-    @PostMapping("/api/users")
+    @PostMapping("/api/users/sign-up")
     @Operation(summary = "회원 가입")
     public ApiResponse<?> signup(
             @RequestBody SignupRequestDto request,
@@ -52,18 +58,18 @@ public class UserController {
         return ApiResponse.ok("회원가입 완료");
     }
 
-    @PutMapping("api/users")
-    @Operation(summary = "회원 수정")
-    public ApiResponse<UserUpdateResponseDto> updateUser(
-            @PathVariable String userId,
-            @RequestBody UserUpdateRequestDto request
-    ) {
+    @PutMapping("/api/users/update")
+        @Operation(summary = "회원 수정")
+        public ApiResponse<UserUpdateResponseDto> updateUser(
+            @RequestBody UserUpdateRequestDto request,
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+            ) {
 
-        userUpdateApplication.update(userId,request);
+        UserUpdateResponseDto result = userUpdateApplication.update(userPrincipal.getUsername(),request);
 
         return ApiResponse.ok(
-                "회원정보가 정상적으로 수정되었습니다.",
-                userUpdateApplication.update(userId, request)
-        );
+                    "회원정보가 정상적으로 수정되었습니다.",
+                    result
+            );
     }
 }
