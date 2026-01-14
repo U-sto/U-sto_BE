05f96eb45db46286da8efcea865190aae0d93a1d
parkdohyun <badberg2002@gmail.com>
2026-01-15 03:10:10 +0900

    서비스 로직

--- src/main/java/com/usto/api/user/application/SignupApplication.java
+++ src/main/java/com/usto/api/user/application/SignupApplication.java
@@ -5,11 +5,16 @@ import com.usto.api.user.domain.model.ApprovalStatus;
 import com.usto.api.user.domain.model.Role;
 import com.usto.api.user.domain.model.User;
 import com.usto.api.user.domain.repository.UserRepository;
+import com.usto.api.user.infrastructure.entity.UserJpaEntity;
 import com.usto.api.user.presentation.dto.request.SignupRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,29 +22,35 @@ public class SignupApplication {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final EmailSendApplication emailSendApplication;
 
     @Transactional
     public void signup(
-            SignupRequestDto request,
+            String UsrId,
+            String UsrNm,
+            String pwd,
+            String orgCd,
             String verifiedEmail,
             String verifiedSms) {
-        String pwHash = passwordEncoder.encode(request.getPwd());
+        String pwHash = passwordEncoder.encode(pwd);
 
         //회원 탈퇴한 회원과 같은 이메일과 전화번호를 쓰려고 하면 막아야한다? 조금 이상한 정책으로 보임,, 어떻게 풀어나가야할지 고민
         //만약에 막아야한다면 여기서 막으면 됌
 
         User user = User.builder()
-                .usrId(request.getUsrId())
-                .usrNm(request.getUsrNm())
+                .usrId(UsrId)
+                .usrNm(UsrNm)
                 .pwHash(pwHash)
                 .email(verifiedEmail)
                 .sms(verifiedSms)
-                .orgCd(request.getOrgCd())
+                .orgCd(orgCd)
                 .apprSts(ApprovalStatus.WAIT)
                 .roleId(Role.GUEST)
                 .build();
 
         userRepository.save(user);
+
+        emailSendApplication.sendApprovalRequestEmail(user);
     }
 }
 
