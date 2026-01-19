bce57ed7de2ade3c8dd0d0bfb2bcf458db9d3e5d
parkdohyun <badberg2002@gmail.com>
2026-01-19 22:15:49 +0900

    회원 수정 미완성 확인 (인증 과정 누락)

--- src/main/java/com/usto/api/user/presentation/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/usto/api/user/presentation/dto/request/UserUpdateRequestDto.java
@@ -10,11 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserUpdateRequestDto {
 
-    @Schema(example = "사용자 이름")
+    //회원 수정에 인증 과정 별도로 구현 필요함 !
+
+    @Schema(example = "홍길동")
     private String newUsrNm;
 
     @Pattern(regexp = "^[0-9]{11}$", message = "전화번호는 숫자 11자리여야 합니다.")
-    @Schema(example = "01000000000")
+    @Schema(example = "01012345678")
     private String newSms;
 
     @Schema(example = "newPw1234!")
