85400397c52ffc0323324a1ddcc6d62b0b5affc2
yeyeongyun <dpdudyyy@gmail.com>
2026-01-19 17:47:43 +0900

    fix: 컨버터 추가, V6 수정

--- src/main/resources/db/migration/V6__create_items.sql
+++ src/main/resources/db/migration/V6__create_items.sql
@@ -3,7 +3,7 @@ use usto;
 
 -- TB_ITEM001M 물품취득기본 (취득 당시 스냅샷)
 CREATE TABLE `TB_ITEM001M` (
-  `ACQ_ID`        BIGINT          NOT NULL AUTO_INCREMENT COMMENT '취득ID',
+  `ACQ_ID`        BINARY(16)      NOT NULL COMMENT '취득ID',
   `G2B_D_CD`      CHAR(8)         NOT NULL COMMENT '물품식별코드',
   `ACQ_AT`        DATE            NOT NULL COMMENT '취득일자',
   `ACQ_UPR`       DECIMAL(20,0)   NOT NULL COMMENT '취득단가(기본)',
@@ -35,7 +35,7 @@ CREATE TABLE `TB_ITEM001M` (
 
 -- TB_ITEM002M 물품대장기본 (취득 묶음 단위로 관리하는 대장 헤더)
 CREATE TABLE `TB_ITEM002M` (
-  `ACQ_ID`        BIGINT          NOT NULL COMMENT '취득ID',
+  `ACQ_ID`        BINARY(16)      NOT NULL COMMENT '취득ID',
   `G2B_D_CD`      CHAR(8)         NOT NULL COMMENT '물품식별코드',
   `QTY`           INT             NOT NULL COMMENT '수량',    -- 일부 물품이 처분될 시 수량 감소 (초기값: TB_ITEM001M의 '취득수량')
   `ACQ_AT`        DATE            NOT NULL COMMENT '취득일자', -- 스냅샷
@@ -58,8 +58,8 @@ CREATE TABLE `TB_ITEM002M` (
 
 -- TB_ITEM002D 물품대장상세 (개별 물품 단위)
 CREATE TABLE `TB_ITEM002D` (
-  `ITM_NO`        BIGINT          NOT NULL AUTO_INCREMENT COMMENT '물품고유번호',
-  `ACQ_ID`        BIGINT          NOT NULL COMMENT '취득ID',
+  `ITM_NO`        VARCHAR(10)     NOT NULL COMMENT '물품고유번호',
+  `ACQ_ID`        BINARY(16)      NOT NULL COMMENT '취득ID',
   `G2B_D_CD`      CHAR(8)         NOT NULL COMMENT '물품식별코드',
   `DEPT_CD`       VARCHAR(50)     NOT NULL COMMENT '운용부서코드',
   `OPER_STS`      VARCHAR(30)     NOT NULL COMMENT '운용상태',  -- ACQ/OPER/RTN/DSU (취득/운용/반납/불용)
@@ -78,14 +78,14 @@ CREATE TABLE `TB_ITEM002D` (
   `DEL_AT`  DATETIME       NULL     COMMENT '삭제일시',
 
   PRIMARY KEY (`ITM_NO`)
-  -- FK: 취득ID, 물품분류코드, 물품식별코드, 운용부서코드, 조직코드
+  -- FK: 취득ID, 물품식별코드, 운용부서코드, 조직코드
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품대장상세';
 
 
 -- TB_ITEM003M/D 물품 반납 (M:신청서, D:상세 물품)
 CREATE TABLE `TB_ITEM003M` (
-  `RTRN_M_ID`     BIGINT          NOT NULL AUTO_INCREMENT COMMENT '반납ID',
+  `RTRN_M_ID`     BINARY(16)      NOT NULL COMMENT '반납ID',
   `APLY_USR_ID`   VARCHAR(30)     NOT NULL COMMENT '등록자ID',
   `APLY_AT`       DATE            NOT NULL COMMENT '반납(등록)일자',
   `ITEM_STS`      VARCHAR(30)     NOT NULL COMMENT '물품상태',  -- NEW/USED/SCRAP (신품/중고품/폐품)
@@ -109,9 +109,9 @@ CREATE TABLE `TB_ITEM003M` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품반납기본';
 
 CREATE TABLE `TB_ITEM003D` (
-  `RTRN_D_ID`     BIGINT          NOT NULL AUTO_INCREMENT COMMENT '반납상세ID',
-  `RTRN_M_ID`     BIGINT          NOT NULL COMMENT '반납ID',
-  `ITM_NO`        BIGINT          NOT NULL COMMENT '물품고유번호',
+  `RTRN_D_ID`     BINARY(16)      NOT NULL COMMENT '반납상세ID',
+  `RTRN_M_ID`     BINARY(16)      NOT NULL COMMENT '반납ID',
+  `ITM_NO`        VARCHAR(10)     NOT NULL COMMENT '물품고유번호',
   `DEPT_CD`       VARCHAR(50)     NOT NULL COMMENT '운용부서코드',  -- 신청 당시의 값이 스냅샷으로 남음
   `ORG_CD`        VARCHAR(50)     NOT NULL COMMENT '조직코드',
     --
@@ -131,7 +131,7 @@ CREATE TABLE `TB_ITEM003D` (
 
 -- TB_ITEM004M/D 물품 불용 (M:신청서, D:상세 물품)
 CREATE TABLE `TB_ITEM004M` (
-  `DSU_M_ID`      BIGINT          NOT NULL AUTO_INCREMENT COMMENT '불용ID',
+  `DSU_M_ID`      BINARY(16)      NOT NULL COMMENT '불용ID',
   `APLY_USR_ID`   VARCHAR(30)     NOT NULL COMMENT '등록자ID',
   `APLY_AT`       DATE            NOT NULL COMMENT '불용(등록)일자',
   `ITEM_STS`      VARCHAR(30)     NOT NULL COMMENT '물품상태',
@@ -155,9 +155,9 @@ CREATE TABLE `TB_ITEM004M` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품불용기본';
 
 CREATE TABLE `TB_ITEM004D` (
-  `DSU_D_ID`      BIGINT          NOT NULL AUTO_INCREMENT COMMENT '불용상세ID',
-  `DSU_M_ID`      BIGINT          NOT NULL COMMENT '불용ID',
-  `ITM_NO`        BIGINT          NOT NULL COMMENT '물품고유번호',
+  `DSU_D_ID`      BINARY(16)      NOT NULL COMMENT '불용상세ID',
+  `DSU_M_ID`      BINARY(16)      NOT NULL COMMENT '불용ID',
+  `ITM_NO`        VARCHAR(10)     NOT NULL COMMENT '물품고유번호',
   `DEPT_CD`       VARCHAR(50)     NOT NULL COMMENT '운용부서코드',  -- 신청 당시의 값이 스냅샷으로 남음
   `ORG_CD`        VARCHAR(50)     NOT NULL COMMENT '조직코드',
     --
@@ -175,10 +175,55 @@ CREATE TABLE `TB_ITEM004D` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품불용상세';
 
 
--- TB_ITEM005M 상태 변경 이력
+-- TB_ITEM005M/D 물품 처분 (M:신청서, D:상세 물품)
 CREATE TABLE `TB_ITEM005M` (
-  `ITEM_HIS_ID`   BIGINT          NOT NULL AUTO_INCREMENT COMMENT '상태이력ID',
-  `ITM_NO`        BIGINT          NOT NULL COMMENT '물품고유번호',
+  `DISP_M_ID`     BINARY(16)      NOT NULL COMMENT '처분ID',
+  `APLY_USR_ID`   VARCHAR(30)     NOT NULL COMMENT '등록자ID',
+  `DISP_TYPE`     VARCHAR(30)     NOT NULL COMMENT '처분방식',
+  `DISP_AT`       DATE            NOT NULL COMMENT '처분일자',
+  `APPR_USR_ID`   VARCHAR(30)     NULL     COMMENT '확정자ID',
+  `APPR_STS`      VARCHAR(30)     NOT NULL DEFAULT 'WAIT' COMMENT '승인상태',
+  `ORG_CD`        VARCHAR(50)     NOT NULL COMMENT '조직코드',
+    --
+  `CRE_BY`  VARCHAR(100)   NOT NULL COMMENT '생성자ID',
+  `CRE_AT`  DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
+  `UPD_BY`  VARCHAR(100)   NULL COMMENT '수정자ID',
+  `UPD_AT`  DATETIME       NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일자',
+    --
+  `DEL_YN`  CHAR(1)        NOT NULL DEFAULT 'N' COMMENT '삭제여부(Y/N)',
+  `DEL_AT`  DATETIME       NULL     COMMENT '삭제일시',
+
+  PRIMARY KEY (`DISP_M_ID`)
+  -- FK: 등록자&확정자 ID, 조직코드
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품처분기본';
+
+CREATE TABLE `TB_ITEM005D` (
+  `DISP_D_ID`     BINARY(16)      NOT NULL COMMENT '처분상세ID',
+  `DISP_M_ID`     BINARY(16)      NOT NULL COMMENT '처분ID',
+  `ITM_NO`        VARCHAR(10)     NOT NULL COMMENT '물품고유번호',
+  `ITEM_STS`      VARCHAR(30)     NOT NULL COMMENT '물품상태', -- 처분 신청 시 해당 물품의 불용기본 테이블의 물품상태 값을 받아옴
+  `CHG_RSN`       VARCHAR(200)    NOT NULL COMMENT '사유',    -- 처분 신청 시 해당 물품의 불용기본 테이블의 사유 값을 받아옴
+  `ORG_CD`        VARCHAR(50)     NOT NULL COMMENT '조직코드',
+    --
+  `CRE_BY`  VARCHAR(100)   NOT NULL COMMENT '생성자ID',
+  `CRE_AT`  DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
+  `UPD_BY`  VARCHAR(100)   NULL COMMENT '수정자ID',
+  `UPD_AT`  DATETIME       NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일자',
+    --
+  `DEL_YN`  CHAR(1)        NOT NULL DEFAULT 'N' COMMENT '삭제여부(Y/N)',
+  `DEL_AT`  DATETIME       NULL     COMMENT '삭제일시',
+
+  PRIMARY KEY (`DISP_D_ID`)
+  -- FK: 처분ID, 물품고유번호, 조직코드
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품처분상세';
+
+
+-- TB_ITEM005M 상태 변경 이력
+CREATE TABLE `TB_ITEM006M` (
+  `ITEM_HIS_ID`   BINARY(16)      NOT NULL COMMENT '상태이력ID',
+  `ITM_NO`        VARCHAR(10)     NOT NULL COMMENT '물품고유번호',
   `PREV_STS`      VARCHAR(30)     NOT NULL COMMENT '이전상태',
   `NEW_STS`       VARCHAR(30)     NOT NULL COMMENT '변경상태',
   `CHG_RSN`       VARCHAR(200)    NULL     COMMENT '변경사유',
@@ -200,48 +245,3 @@ CREATE TABLE `TB_ITEM005M` (
   -- FK: 물품고유번호, 등록자&확정자 ID, 조직코드
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품상태이력';
-
-
--- TB_ITEM006M/D 물품 처분 (M:신청서, D:상세 물품)
-CREATE TABLE `TB_ITEM006M` (
-  `DISP_M_ID`     BIGINT          NOT NULL AUTO_INCREMENT COMMENT '처분ID',
-  `APLY_USR_ID`   VARCHAR(30)     NOT NULL COMMENT '등록자ID',
-  `DISP_TYPE`     VARCHAR(30)     NOT NULL COMMENT '처분방식',
-  `DISP_AT`       DATE            NOT NULL COMMENT '처분일자',
-  `APPR_USR_ID`   VARCHAR(30)     NULL     COMMENT '확정자ID',
-  `APPR_STS`      VARCHAR(30)     NOT NULL DEFAULT 'WAIT' COMMENT '승인상태',
-  `ORG_CD`        VARCHAR(50)     NOT NULL COMMENT '조직코드',
-    --
-  `CRE_BY`  VARCHAR(100)   NOT NULL COMMENT '생성자ID',
-  `CRE_AT`  DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
-  `UPD_BY`  VARCHAR(100)   NULL COMMENT '수정자ID',
-  `UPD_AT`  DATETIME       NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일자',
-    --
-  `DEL_YN`  CHAR(1)        NOT NULL DEFAULT 'N' COMMENT '삭제여부(Y/N)',
-  `DEL_AT`  DATETIME       NULL     COMMENT '삭제일시',
-
-  PRIMARY KEY (`DISP_M_ID`)
-  -- FK: 등록자&확정자 ID, 조직코드
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품처분기본';
-
-CREATE TABLE `TB_ITEM006D` (
-  `DISP_D_ID`     BIGINT          NOT NULL AUTO_INCREMENT COMMENT '처분상세ID',
-  `DISP_M_ID`     BIGINT          NOT NULL COMMENT '처분ID',
-  `ITM_NO`        BIGINT          NOT NULL COMMENT '물품고유번호',
-  `ITEM_STS`      VARCHAR(30)     NOT NULL COMMENT '물품상태', -- 처분 신청 시 해당 물품의 불용기본 테이블의 물품상태 값을 받아옴
-  `CHG_RSN`       VARCHAR(200)    NOT NULL COMMENT '사유',    -- 처분 신청 시 해당 물품의 불용기본 테이블의 사유 값을 받아옴
-  `ORG_CD`        VARCHAR(50)     NOT NULL COMMENT '조직코드',
-    --
-  `CRE_BY`  VARCHAR(100)   NOT NULL COMMENT '생성자ID',
-  `CRE_AT`  DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
-  `UPD_BY`  VARCHAR(100)   NULL COMMENT '수정자ID',
-  `UPD_AT`  DATETIME       NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일자',
-    --
-  `DEL_YN`  CHAR(1)        NOT NULL DEFAULT 'N' COMMENT '삭제여부(Y/N)',
-  `DEL_AT`  DATETIME       NULL     COMMENT '삭제일시',
-
-  PRIMARY KEY (`DISP_D_ID`)
-  -- FK: 처분ID, 물품고유번호, 조직코드
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='물품처분상세';
