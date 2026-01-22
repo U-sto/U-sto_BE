45f70264953168d934933206c443b88994019dfe
parkdohyun <badberg2002@gmail.com>
2026-01-22 18:51:30 +0900

    스케줄러 Config

---
+++ src/main/java/com/usto/api/common/utils/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.usto.api.common.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig{
+}
