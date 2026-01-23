8048916786e02a489b35ec78be70a4e0d4daac1d
parkdohyun <badberg2002@gmail.com>
2026-01-23 22:00:57 +0900

    API 관련

--- src/main/java/com/usto/api/common/utils/ItemsFlexibleDeserializer.java
+++ src/main/java/com/usto/api/common/utils/ItemsFlexibleDeserializer.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.usto.api.g2b.presentation.dto.response.ShoppingMallEnvelope;
 
 import java.io.IOException;
 import java.util.List;
