68dcc131fb07bfef00cf2653748bb6446d8d662c
parkdohyun <badberg2002@gmail.com>
2026-01-23 21:58:58 +0900

    자동화 구축

--- src/main/java/com/usto/api/common/utils/G2bDailySyncJob.java
+++ src/main/java/com/usto/api/common/utils/G2bDailySyncJob.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-
 @Service
 @RequiredArgsConstructor
 public class G2bDailySyncJob {
@@ -16,7 +14,6 @@ public class G2bDailySyncJob {
     // 매일 00:01 (Asia/Seoul)
     @Scheduled(cron = "0 01 0 * * *", zone = "Asia/Seoul")
     public void runDaily() {
-        LocalDate target = LocalDate.now().minusDays(1); // 보통 어제분 (당일 데이터 지연 고려)
-        g2bSyncServiceImpl.syncDaily(target);
+        g2bSyncServiceImpl.syncDaily();
     }
 }
