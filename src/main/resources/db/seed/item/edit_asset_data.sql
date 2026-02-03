ab049e1e80cd5d1c023d68cdb57dbe5b9c7bbff8
parkdohyun <badberg2002@gmail.com>
2026-02-04 01:16:34 +0900

    [test] 중요X

--- src/main/resources/db/seed/item/edit_asset_data.sql
+++ src/main/resources/db/seed/item/edit_asset_data.sql
@@ -28,3 +28,35 @@ SELECT
     APPR_STS,
     ACQ_AT
 FROM TB_ITEM001M;
+
+select * from TB_ITEM002D;
+
+SELECT
+    d.ITM_NO,
+    d.ORG_CD,
+    g.G2B_D_NM,
+    CONCAT(c.G2B_M_CD, '-', g.G2B_D_CD) AS g2bItemNo,
+    d.ACQ_UPR,
+    m.ACQ_AT,
+    m.ARRG_AT,
+    d.OPER_STS,
+    d.DRB_YR,
+    dept.DEPT_NM,
+    m.QTY,
+    d.RMK
+FROM TB_ITEM002D d
+         INNER JOIN TB_ITEM002M m
+                    ON m.ACQ_ID = d.ACQ_ID
+                        AND m.DEL_YN = 'N'
+         INNER JOIN TB_G2B001D g
+                    ON g.G2B_D_CD = d.G2B_D_CD
+         INNER JOIN TB_G2B001M c
+                    ON c.G2B_M_CD = g.G2B_M_CD
+         INNER JOIN TB_ORG002M dept
+                    ON dept.DEPT_CD = d.DEPT_CD
+                        AND dept.ORG_CD = d.ORG_CD
+WHERE d.ITM_NO = 'M202600001'
+  AND d.ORG_CD = '7008277'
+  AND d.DEL_YN = 'N'
+  AND m.DEL_YN = 'N'
+;
