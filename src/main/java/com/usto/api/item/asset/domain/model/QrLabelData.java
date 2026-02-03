1fa3264c93d8aa2f13a61633cbebb754e2dfce6c
parkdohyun <badberg2002@gmail.com>
2026-02-03 23:19:53 +0900

    [feature] PDF내용과 생성기 구현

---
+++ src/main/java/com/usto/api/item/asset/domain/model/QrLabelData.java
@@ -0,0 +1,16 @@
+package com.usto.api.item.asset.domain.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * QR 라벨 출력을 위한 데이터 모델
+ * - 라벨에는 물품고유번호만 표시
+ * - QR 코드는 상세 페이지 URL 포함
+ */
+@Getter
+@Builder
+public class QrLabelData {
+    private String itmNo;           // 물품고유번호 (라벨에 표시)
+    private String qrContent;       // QR 코드 내용 (URL)
+}
