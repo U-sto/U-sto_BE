93b9caa08df6996372c1a871eff2eb28b29aa6f9
yeyeongyun <dpdudyyy@gmail.com>
2026-02-27 17:31:48 +0900

    feature: ngrok 가입 후 테스트용 고정주소 추가

--- src/main/java/com/usto/api/common/config/SecurityConfig.java
+++ src/main/java/com/usto/api/common/config/SecurityConfig.java
@@ -149,7 +149,9 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://localhost:8080",
                 "http://localhost:5500", //로컬 테스트용
-                "https://avengeful-shaunte-revolvingly.ngrok-free.dev" //로컬 테스트용
+                "http://localhost:5173", // 프론트(Vite) 로컬 테스트용
+                "https://avengeful-shaunte-revolvingly.ngrok-free.dev", //로컬 테스트용
+                "https://shavable-shella-thumbless.ngrok-free.dev"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
