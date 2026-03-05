8a6cc162dabde291d71b1126e71400bcdb11a3c9
parkdohyun <badberg2002@gmail.com>
2026-03-05 21:12:32 +0900

    [refactor] dev랑 prod 구분 작업

--- src/main/java/com/usto/api/common/utils/SessionExistsHeaderFilter.java
+++ src/main/java/com/usto/api/common/utils/SessionExistsHeaderFilter.java
@@ -10,7 +10,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Profile("dev")
 @Component
 public class SessionExistsHeaderFilter extends OncePerRequestFilter {
 
