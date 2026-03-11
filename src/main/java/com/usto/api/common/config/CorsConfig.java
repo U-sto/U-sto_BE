d3a0e1d897773dffe604c76a33b5da2badd26001
park, do hyun <90432955+DoHyunBak@users.noreply.github.com>
2026-03-11 17:39:06 +0900

    [fix] cors 설정 (#119)

--- src/main/java/com/usto/api/common/config/CorsConfig.java
+++ src/main/java/com/usto/api/common/config/CorsConfig.java
@@ -17,8 +17,11 @@ public class CorsConfig {
                         .allowedOrigins(
                                 "https://u-sto.github.io",
                                 "http://localhost:3000", // 필요 시 로컬 프론트 테스트용
+                                "http://localhost:8080", // 필요 시 백엔드 프론트 테스트용
+                                "http://localhost:5173", // 필요 시 로컬 프론트 테스트용
                                 "http://13.124.10.41:3000",
-                                "http://13.124.10.41:8080"
+                                "http://13.124.10.41:8080",
+                                "http://13.124.10.41:5173"
                         )
                         .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                         .allowedHeaders("*");
